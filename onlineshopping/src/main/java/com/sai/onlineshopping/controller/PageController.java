d5e0af70e2d582b907919b41a8dfb77336ec6e75
Sai Kiran <sskr993@gmail.com>
2018-02-01 16:49:49 +0530

    Successfully Loaded Static Resources

--- onlineshopping/src/main/java/com/sai/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/sai/onlineshopping/controller/PageController.java
@@ -1,7 +1,9 @@
 package com.sai.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -13,5 +15,14 @@ public class PageController {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("greeting", "Welcome to Spring Web MVC");
 		return mv;
+	}
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable(value="greeting") String greeting)
+	{
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+		
 	}
 }
